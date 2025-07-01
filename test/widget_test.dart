2992c70385f02c679d3900c4e9e63503943b2bad
Korya25 <????? ??? GitHub>
2025-07-01 10:43:41 +0300

    import myapp in test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,10 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:elwekala/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:elwekala/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
