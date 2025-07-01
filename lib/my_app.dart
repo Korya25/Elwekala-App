919fdc7edc6befa6a86a5aa370dfe6bd445d33ae
Korya25 <????? ??? GitHub>
2025-07-01 11:03:46 +0300

    refactor my app to include core files and fintish base app

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,11 +1,25 @@
+import 'package:elwekala/core/resources/app_router.dart';
 import 'package:elwekala/core/resources/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: AppTheme.lightTheme);
+    return ScreenUtilInit(
+      designSize: Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp.router(
+          title: 'Auth App',
+          theme: AppTheme.lightTheme,
+          routerConfig: AppRouter.router,
+          debugShowCheckedModeBanner: false,
+        );
+      },
+    );
   }
 }
