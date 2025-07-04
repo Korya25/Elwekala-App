9c1f08f738d1acf59473b979c5835be882d5f17b
Korya25 <????? ??? GitHub>
2025-07-04 22:15:06 +0300

    trigeer fet favorite cubit

--- lib/features/favorites/presentation/screens/favorites_screen.dart
+++ lib/features/favorites/presentation/screens/favorites_screen.dart
@@ -1,99 +1,68 @@
+import 'package:dio/dio.dart';
+import 'package:elwekala/core/api/dio_consumer.dart';
+import 'package:elwekala/core/utils/app_dialogs.dart';
+import 'package:elwekala/features/favorites/data/dataSources/favorit_remote_data_source.dart';
+import 'package:elwekala/features/favorites/data/repos/get_favorite_repo_impl.dart';
+import 'package:elwekala/features/favorites/domain/useCase/favorite_use_case.dart';
+import 'package:elwekala/features/favorites/presentation/controllers/favorite_cubit.dart';
+import 'package:elwekala/features/favorites/presentation/controllers/favorite_state.dart';
 import 'package:elwekala/features/favorites/presentation/widgets/empty_favorite.dart';
-import 'package:elwekala/features/home/domain/entities/product.dart';
 import 'package:elwekala/features/home/presentation/widgets/sliver_grid_products.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FavoritesScreen extends StatelessWidget {
   const FavoritesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final List<Product> favoriteProducts = [
-      Product(
-        id: "sdfgc",
-        status: "New",
-        category: 'Dell',
-        name: 'Dell 13',
-        price: 30,
-        description:
-            "The Dell XPS 13 is a sleek and powerful laptop designed for productivity and portability. It features an 11th Generation Intel Core processor",
-        image:
-            "https://res.cloudinary.com/dzh2hde2n/image/upload/v1684434230/hsincqjvphkmfbrcoc2b.png",
-        images: [
-          "https://res.cloudinary.com/dzh2hde2n/image/upload/v1684434230/hsincqjvphkmfbrcoc2b.png",
-          "https://res.cloudinary.com/dzh2hde2n/image/upload/v1684434232/ks4tuogejkow8dxw2jdw.png",
-          "https://res.cloudinary.com/dzh2hde2n/image/upload/v1684434234/rjixaryvwv2pykiolblg.png",
-        ],
-        company: "Dell",
-        countInStock: 8,
-        sales: 9,
-      ),
-      Product(
-        id: "sdfgc",
-        status: "New",
-        category: 'Dell',
-        name: 'Dell 13',
-        price: 30,
-        description:
-            "The Dell XPS 13 is a sleek and powerful laptop designed for productivity and portability. It features an 11th Generation Intel Core processor",
-        image:
-            "https://res.cloudinary.com/dzh2hde2n/image/upload/v1684434230/hsincqjvphkmfbrcoc2b.png",
-        images: [
-          "https://res.cloudinary.com/dzh2hde2n/image/upload/v1684434230/hsincqjvphkmfbrcoc2b.png",
-          "https://res.cloudinary.com/dzh2hde2n/image/upload/v1684434232/ks4tuogejkow8dxw2jdw.png",
-          "https://res.cloudinary.com/dzh2hde2n/image/upload/v1684434234/rjixaryvwv2pykiolblg.png",
-        ],
-        company: "Dell",
-        countInStock: 8,
-        sales: 9,
-      ),
-      Product(
-        id: "sdfgc",
-        status: "New",
-        category: 'Dell',
-        name: 'Dell 13',
-        price: 30,
-        description:
-            "The Dell XPS 13 is a sleek and powerful laptop designed for productivity and portability. It features an 11th Generation Intel Core processor",
-        image:
-            "https://res.cloudinary.com/dzh2hde2n/image/upload/v1684434230/hsincqjvphkmfbrcoc2b.png",
-        images: [
-          "https://res.cloudinary.com/dzh2hde2n/image/upload/v1684434230/hsincqjvphkmfbrcoc2b.png",
-          "https://res.cloudinary.com/dzh2hde2n/image/upload/v1684434232/ks4tuogejkow8dxw2jdw.png",
-          "https://res.cloudinary.com/dzh2hde2n/image/upload/v1684434234/rjixaryvwv2pykiolblg.png",
-        ],
-        company: "Dell",
-        countInStock: 8,
-        sales: 9,
-      ),
-      Product(
-        id: "sdfgc",
-        status: "New",
-        category: 'Dell',
-        name: 'Dell 13',
-        price: 30,
-        description:
-            "The Dell XPS 13 is a sleek and powerful laptop designed for productivity and portability. It features an 11th Generation Intel Core processor",
-        image:
-            "https://res.cloudinary.com/dzh2hde2n/image/upload/v1684434230/hsincqjvphkmfbrcoc2b.png",
-        images: [
-          "https://res.cloudinary.com/dzh2hde2n/image/upload/v1684434230/hsincqjvphkmfbrcoc2b.png",
-          "https://res.cloudinary.com/dzh2hde2n/image/upload/v1684434232/ks4tuogejkow8dxw2jdw.png",
-          "https://res.cloudinary.com/dzh2hde2n/image/upload/v1684434234/rjixaryvwv2pykiolblg.png",
-        ],
-        company: "Dell",
-        countInStock: 8,
-        sales: 9,
-      ),
-    ];
-
-    return Scaffold(
-      appBar: AppBar(title: Text('My Favorites')),
-      body: favoriteProducts.isEmpty
-          ? EmptyFavoriteList()
-          : CustomScrollView(
-              slivers: [SliverGridProducts(products: favoriteProducts)],
+    return BlocProvider(
+      create: (_) => FavoriteCubit(
+        FavoriteUseCase(
+          GetFavoriteRepoImpl(
+            favoriteRemoteDataSource: FavoriteRemoteDataSourceImpl(
+              apiConsumer: DioConsumer(dio: Dio()),
             ),
+          ),
+        ),
+      )..getFavorite(nationalId: "01009876567876"),
+      child: Scaffold(
+        appBar: AppBar(title: const Text('My Favorites')),
+        body: const FavoritesScreenBody(),
+      ),
+    );
+  }
+}
+
+class FavoritesScreenBody extends StatelessWidget {
+  const FavoritesScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<FavoriteCubit, FavoriteState>(
+      listener: (context, state) {
+        if (state is FavoriteErrorState) {
+          AppDialogs.showError(context, state.error);
+        }
+      },
+      builder: (context, state) {
+        if (state is FavoriteLoadingState) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (state is FavoriteGetSuccesState) {
+          if (state.favoriteProduct.isEmpty) {
+            return const EmptyFavoriteList();
+          }
+
+          return CustomScrollView(
+            physics: const BouncingScrollPhysics(),
+            slivers: [SliverGridProducts(products: state.favoriteProduct)],
+          );
+        }
+
+        return const SizedBox();
+      },
     );
   }
 }
--- lib/features/home/presentation/widgets/sliver_grid_products.dart
+++ lib/features/home/presentation/widgets/sliver_grid_products.dart
@@ -4,17 +4,21 @@ import 'package:elwekala/features/home/presentation/widgets/product_card.dart';
 import 'package:elwekala/features/home/presentation/widgets/sliver_grid_animation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:go_router/go_router.dart';
 
 class SliverGridProducts extends StatelessWidget {
-  const SliverGridProducts({super.key, required this.products});
-
   final List<Product> products;
+  const SliverGridProducts({super.key, required this.products});
 
   @override
   Widget build(BuildContext context) {
-    return SliverGrid(
-      delegate: SliverChildBuilderDelegate((context, index) {
+    return SliverMasonryGrid.count(
+      crossAxisCount: 2,
+      mainAxisSpacing: 16.h,
+      crossAxisSpacing: 16.w,
+      childCount: products.length,
+      itemBuilder: (context, index) {
         final product = products[index];
         return SliverGridAnimation(
           productIndex: index,
@@ -24,17 +28,7 @@ class SliverGridProducts extends StatelessWidget {
                 context.pushNamed(AppRoutes.productDetail, extra: product),
           ),
         );
-      }, childCount: products.length),
-      gridDelegate: buildGridDelegate(),
-    );
-  }
-
-  SliverGridDelegateWithFixedCrossAxisCount buildGridDelegate() {
-    return SliverGridDelegateWithFixedCrossAxisCount(
-      crossAxisCount: 2,
-      crossAxisSpacing: 16.w,
-      mainAxisSpacing: 16.h,
-      childAspectRatio: 0.7,
+      },
     );
   }
 }
